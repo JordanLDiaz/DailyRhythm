aa73c21f8cd07d52d1ce406509da8df185cd7597
Jordan Diaz <jordanldiaz29@gmail.com>
2023-03-30 09:58:01 -0600

    stuff

--- DailyRhythm.client/src/env.js
+++ DailyRhythm.client/src/env.js
@@ -1,9 +1,9 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = 'bryan-vassar.us.auth0.com'
-export const clientId = 'wbVaUZk4wdIqMXWLCnNUKKznQbywQkzf'
-export const audience = 'https://bryanclassroom.com'
+export const domain = 'dev-wra2r4u13787lot5.us.auth0.com'
+export const clientId = 'Wcq0CtKAqonSFxcd1VwYcURAxkuwcs8t'
+export const audience = 'https://classroom.com'
 //Weather API key
 export const appid = 'cff3a95bc2ea79071a521f836aa63be6'
 //Unsplash background image API key
